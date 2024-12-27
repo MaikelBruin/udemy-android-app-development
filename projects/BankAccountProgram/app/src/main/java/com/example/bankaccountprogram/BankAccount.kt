b3543b5691df1ab2486aa12e10936436d03ec255
MB <>
2024-12-27 15:45:49 +0100

    bank account program

---
+++ projects/BankAccountProgram/app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
@@ -0,0 +1,30 @@
+package com.example.bankaccountprogram
+
+class BankAccount(
+    var accountHolder: String,
+    var balance: Double
+) {
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount: Double) {
+        balance += amount
+        transactionHistory.add("$accountHolder deposited $$amount")
+    }
+
+    fun withdraw(amount: Double) {
+        if (amount <= balance) {
+            balance -= amount
+            transactionHistory.add("$accountHolder withdrew $$amount")
+        } else {
+            println("Not enough funds")
+        }
+    }
+
+    fun displayBalance() {
+        println("balance of $accountHolder is $balance")
+    }
+
+    fun displayTransactionHistory() {
+        println(transactionHistory)
+    }
+}
---
+++ projects/BankAccountProgram/app/src/main/java/com/example/bankaccountprogram/BankMain.kt
@@ -0,0 +1,18 @@
+package com.example.bankaccountprogram
+
+fun main() {
+    val bankAccount = BankAccount("MB", 1000.0)
+    println(bankAccount.displayBalance())
+    println(bankAccount.displayTransactionHistory())
+
+    bankAccount.deposit(111.0)
+    println(bankAccount.displayBalance())
+    println(bankAccount.displayTransactionHistory())
+
+    bankAccount.withdraw(111.0)
+    println(bankAccount.displayBalance())
+    println(bankAccount.displayTransactionHistory())
+    bankAccount.withdraw(2000.0)
+    println(bankAccount.displayTransactionHistory())
+
+}
