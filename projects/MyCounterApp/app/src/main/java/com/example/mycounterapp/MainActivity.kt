419729d17acf2229cd4818702682071203107809
MB <>
2024-12-30 16:35:22 +0100

    still need to add the dependency

--- projects/MyCounterApp/app/src/main/java/com/example/mycounterapp/MainActivity.kt
+++ projects/MyCounterApp/app/src/main/java/com/example/mycounterapp/MainActivity.kt
@@ -3,24 +3,23 @@ package com.example.mycounterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycounterapp.ui.theme.MyCounterAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = CounterViewModel()
         setContent {
+            val viewModel: CounterViewModel = CounterViewModel()
             MyCounterAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -35,14 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CounterApp(counterViewModel: CounterViewModel) {
-    Column {
-        Button(onClick = { counterViewModel.increment() }) {
+fun CounterApp(viewModel: CounterViewModel) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { viewModel.increment() }) {
             Text(text = "Increment")
         }
-        Button(onClick = { counterViewModel.decrement() }) {
+        Button(onClick = { viewModel.decrement() }) {
             Text(text = "Decrement")
         }
-        Text(text = "Count: ${counterViewModel.count.value}")
+        Text(text = "Count: ${viewModel.count.value}")
     }
 }
