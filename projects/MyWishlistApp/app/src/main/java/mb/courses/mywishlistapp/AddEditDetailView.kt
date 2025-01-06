cec82be856e402fd0d82ee34f6de68dd63716e24
MB <>
2025-01-06 19:06:20 +0100

    add view

--- projects/MyWishlistApp/app/src/main/java/mb/courses/mywishlistapp/AddEditDetailView.kt
+++ projects/MyWishlistApp/app/src/main/java/mb/courses/mywishlistapp/AddEditDetailView.kt
@@ -3,15 +3,24 @@ package mb.courses.mywishlistapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -41,5 +50,33 @@ fun AddEditDetailView(
 
         }
     }
+}
+
+
+@Composable
+fun WishTextField(
+    label: String,
+    value: String,
+    onValueChanged: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(),
+        value = value,
+        onValueChange = onValueChanged,
+        label = {
+            Text(
+                text = label,
+                color = Color.Black
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+fun WishTextFieldPreview() {
+    WishTextField(label = "text", value = "value", onValueChanged = {})
 }
 
