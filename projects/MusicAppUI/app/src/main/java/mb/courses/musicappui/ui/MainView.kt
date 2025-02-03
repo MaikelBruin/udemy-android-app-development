a09dca7263bbcf40815fca0626bbe46f1cc350be
MB <>
2025-02-03 20:24:03 +0100

    add composable for drawerItem

--- projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/ui/MainView.kt
+++ projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/ui/MainView.kt
@@ -1,5 +1,9 @@
 package mb.courses.musicappui.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
@@ -8,14 +12,19 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import mb.courses.musicappui.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,5 +51,30 @@ fun MainView() {
         }
     ) {
         Text("Text", modifier = Modifier.padding(it))
+    }
+}
+
+@Composable
+fun DrawerItem(
+    selected: Boolean,
+    item: Screen.DrawerScreen,
+    onDrawItemClicked: () -> Unit
+) {
+    val background = if (selected) Color.DarkGray else Color.White
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable {
+                onDrawItemClicked()
+            }) {
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(text = item.dTitle, style = MaterialTheme.typography.h5)
     }
 }
