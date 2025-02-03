6046bf71c97e0fd8ffbbc5d7382c0f9cc2c3ce35
MB <>
2025-02-03 20:32:39 +0100

    implement drawer

--- projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/Screen.kt
+++ projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/Screen.kt
@@ -27,3 +27,9 @@ sealed class Screen(
         )
     }
 }
+
+val screensInDrawer = listOf(
+    Screen.DrawerScreen.Account,
+    Screen.DrawerScreen.Subscription,
+    Screen.DrawerScreen.AddAccount
+)
--- projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/ui/MainView.kt
+++ projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/ui/MainView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -13,18 +15,25 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import mb.courses.musicappui.Screen
+import mb.courses.musicappui.screensInDrawer
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,6 +41,13 @@ fun MainView() {
 
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    val title = remember {
+        //TODO: change to currentScreen.title
+        mutableStateOf("")
+    }
 
 
     Scaffold(
@@ -48,6 +64,23 @@ fun MainView() {
                         Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
                     }
                 })
+        }, scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)) {
+                items(screensInDrawer) { item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        if (item.dRoute == "add_account") {
+                            //open dialog
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
         }
     ) {
         Text("Text", modifier = Modifier.padding(it))
