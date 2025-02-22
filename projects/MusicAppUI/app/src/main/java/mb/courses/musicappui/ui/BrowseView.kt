c55cec7004fb59ae9bb31ba6175f3e56a4689d02
MB <>
2025-02-22 09:34:09 +0100

    use BrowserItem instead of text

--- projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/ui/BrowseView.kt
+++ projects/MusicAppUI/app/src/main/java/mb/courses/musicappui/ui/BrowseView.kt
@@ -4,16 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import mb.courses.musicappui.R
 
 @Composable
 fun BrowseView() {
     val categories = listOf("Pop", "Funk", "Hiphop", "House", "Techno", "Electro")
     LazyVerticalGrid(columns = GridCells.Fixed(2), horizontalArrangement = Arrangement.Center) {
         items(categories) { cat ->
-            Text(text = cat)
+            BrowserItem(cat = cat, drawable = R.drawable.baseline_apps_24)
         }
     }
 }
